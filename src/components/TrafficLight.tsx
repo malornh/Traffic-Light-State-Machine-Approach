957527620e56a3d11d32829bf45da4a090e2d6d5
Jivko Ivanov <jivko.ivanov14@gmail.com>
2025-03-09 22:44:58 +0200

    import the css FFS

--- src/components/TrafficLight.tsx
+++ src/components/TrafficLight.tsx
@@ -1,4 +1,5 @@
 import { TrafficLightStates } from "./states/TrafficLightStates";
+import "./TrafficLight.css";
 
 interface TrafficLightProps {
   state: any;
@@ -12,16 +13,16 @@ const TrafficLight: React.FC<TrafficLightProps> = ({ state }) => {
         className={`light ${
           state.matches(TrafficLightStates.RED) ||
           state.matches(TrafficLightStates.RED_YELLOW) ? 'red' : 'off'}`}
-      />
-    <div
-      className={`light 
-        ${state.matches(TrafficLightStates.RED_YELLOW) ? 'yellow' : ''} 
-        ${state.matches(TrafficLightStates.BROKEN) ? 'broken' : ''} 
-        ${!state.matches(TrafficLightStates.RED_YELLOW) && !state.matches(TrafficLightStates.BROKEN) ? 'off' : ''}`
-      }
-    />
+       />
       <div
-        className={`light ${state.matches(TrafficLightStates.GREEN) ? 'green' : 'off'}`}
+        className={`light 
+          ${state.matches(TrafficLightStates.RED_YELLOW) ? 'yellow' : ''} 
+          ${state.matches(TrafficLightStates.BROKEN) ? 'broken' : ''} 
+          ${!state.matches(TrafficLightStates.RED_YELLOW) && !state.matches(TrafficLightStates.BROKEN) ? 'off' : ''}`
+        }
+      />
+      <div
+          className={`light ${state.matches(TrafficLightStates.GREEN) ? 'green' : 'off'}`}
       />
     </div>
   );
